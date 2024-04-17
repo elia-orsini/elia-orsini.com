30df6404055d05f532b8684680f09e2e5e1a5fa2
Your name here <elia.orsini@hotmail.com>
2024-04-17 10:30:27 +0100

    fix: reordering of metal

--- pages/design/metal.tsx
+++ pages/design/metal.tsx
@@ -11,12 +11,13 @@ export default function TwoD() {
         <div className="flex-col mx-auto w-4/5 my-20">
           <div className="flex-col mx-auto grid md:grid-cols-3 mt-5 gap-2">
             <div className="flex flex-col space-y-2">
-              <Image src="/design/metal/3.jpg" />
-              <Image src="/design/metal/4.jpg" />
+              <Image src="/design/metal/39.jpg" />
               <Image src="/design/metal/5.jpg" />
+              <Image src="/design/metal/3.jpg" />
               <Image src="/design/metal/22.jpg" />
               <Image src="/design/metal/34.jpg" />
               <Image src="/design/metal/6.jpg" />
+              <Image src="/design/metal/4.jpg" />
               <Image src="/design/metal/26.jpg" />
               <Image src="/design/metal/30.jpg" />
               <Image src="/design/metal/37.jpg" />
@@ -24,11 +25,11 @@ export default function TwoD() {
             </div>
             <div className="flex flex-col space-y-2">
               <Image src="/design/metal/9.jpg" />
-              <Image src="/design/metal/10.jpg" />
-              <Image src="/design/metal/11.jpg" />
               <Image src="/design/metal/35.jpg" />
-              <Image src="/design/metal/8.jpg" />
               <Image src="/design/metal/13.jpg" />
+              <Image src="/design/metal/11.jpg" />
+              <Image src="/design/metal/8.jpg" />
+              <Image src="/design/metal/10.jpg" />
               <Image src="/design/metal/14.jpg" />
               <Image src="/design/metal/28.jpg" />
               <Image src="/design/metal/32.jpg" />
@@ -38,6 +39,7 @@ export default function TwoD() {
             <div className="flex flex-col space-y-2">
               <Image src="/design/metal/12.jpg" />
               <Image src="/design/metal/15.jpg" />
+              <Image src="/design/metal/38.jpg" />
               <Image src="/design/metal/17.jpg" />
               <Image src="/design/metal/16.jpg" />
               <Image src="/design/metal/36.jpg" />
