331b9241336e1ce16aead870fdb9886c80799527
Your name here <elia.orsini@hotmail.com>
2024-05-02 09:37:02 +0100

    feat: add instruction on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,11 @@ const Scene = () => {
 export default function Entrance() {
   return (
     <Link href="/work">
+      <div className="absolute flex w-screen h-screen z-20">
+        <p className="mx-auto mt-[80vh] text-white font-mono text-xs sm:text-sm">
+          {"["}click anywhere to enter{"]"}
+        </p>
+      </div>
       <div className="flex">
         <div className="bg-white w-full min-h-screen">
           <Canvas className="z-10">
