a1bc73b4ca87d6ee3ba1a7546d70c6484c58ae43
Your name here <elia.orsini@hotmail.com>
2025-11-10 10:00:00 +0800

    feat: capitalize and improve seo of sunshi

--- pages/design/sunshi.tsx
+++ pages/design/sunshi.tsx
@@ -9,7 +9,12 @@ const koulen = Koulen({ weight: "400", subsets: ["latin"] });
 export default function TwoD() {
   return (
     <>
-      <Header title={`Sunshi`} />
+      <Header
+        title={`Sunshi`}
+        description="The sun always shines in the city of Sunshi. Thanks to the latest developments in solar energy, an artificial sun has been installed
+            to light people's lives. It regulates the temperature and provides a warm and delicate light. and when it rains,
+            the sky is full of rainbows and kids can play in the streets."
+      />
 
       <Navigation>
         <div className="mx-auto mt-20 w-4/5 flex-col justify-between md:mx-0">
@@ -18,20 +23,20 @@ export default function TwoD() {
             The sun always shines in the city of Sunshi. <br />
             Thanks to the latest developments in solar energy, an artificial sun has been installed
             to light people&apos;s lives. <br />
-            It regulates the temperature and provides a warm and delicate light. and when it rains,
+            It regulates the temperature and provides a warm and delicate light. And when it rains,
             the sky is full of rainbows and kids can play in the streets.
             <br />
             After a difficult decade, people started to leave their screens and go out for long
-            walks. the city is now vibrant with life.
+            walks. The city is now vibrant with life.
             <br />
             Every day is a sunny sunday morning.
             <br />
-            People are happier and they walk to work. and after they are done, they have their
-            dinner on the roofs of those buildings.
+            People are happier and they walk to work. And after they are done, they have their
+            dinner on the roofs of those same buildings.
             <br />
-            They sunbathe while they eat the fruits of that light. every corner of the city is
+            They sunbathe while they eat the fruits of that light. Every corner of the city is
             tinted by a delicate palette of warm colours. <br />
-            everything worked out in the end.
+            Everything worked out in the end.
           </p>
 
           <div className="mx-auto mt-5 grid w-full flex-col gap-2 sm:grid-cols-2 lg:grid-cols-3">
